728e95def5fc5f0b6823055ca6dcf4b8111c6be0
yan9yu <dev.eric.young@gmail.com>
2019-03-26 23:04:34 +1300

    remove comments

--- install.sh
+++ install.sh
@@ -12,14 +12,99 @@ brew tap homebrew/cask-fonts
 # brew doctor
 brew doctor
 
-# zsh
+
+#########  APPLICATION  #########
+
+# Quick Look plugins
+brew cask install \
+  qlcolorcode \
+  qlstephen \
+  qlmarkdown \
+  quicklook-json \
+  qlprettypatch \
+  quicklook-csv \
+  betterzip \
+  webpquicklook \
+  suspicious-package
+
+
+# Common Application
+brew cask install \
+  alfred \
+  caffeine \
+  cheatsheet \
+  chromium \
+  dropbox \
+  google-chrome \
+  istat-menus \
+  keka \
+  pdf-expert \
+  proxifier \
+  wechat \
+  vlc \
+  1password
+
+
+# Application Toolkit
+brew cask install \
+  anaconda \
+  docker \
+  intellij-idea-ce \
+  iterm2 \
+  keepassx \
+  sourcetree \
+  sublime-text \
+  virtualbox \
+  visual-studio-code
+
+
+# Command Line Toolkit
+brew install -with-default-names 
+  aria2 \
+  awscli \
+  coreutils 
+  findutils 
+  gawk 
+  gnutls 
+  gnu-getopt 
+  gnu-indent 
+  gnu-tar 
+  gnu-sed 
+  htop \
+  imagemagick \
+  jq \
+  mas \
+  screen \
+  tmux \
+  tree \
+  wget \
+  xclip \
+  you-get
+
+
+#########  MAC APP STORE  #########
+
+mas install 
+  497799835 \
+  540348655 \
+  1176895641 \
+
+
+#########  ZSH  #########
+
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # change default shell to zsh
 chsh -s $(which zsh)
 
 
-# iTerms2
+# add syntax highlighting
+brew install zsh-syntax-highlighting
+echo '\n#[CUSTOM] zsh syntax highlight config\nsource /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+
+
+#########  ITERMS2  #########
 
 # add Solarized Dark theme
 #curl -o /tmp/solarized-dark-patched.itermcolors https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20-%20Patched.itermcolors
@@ -45,88 +130,8 @@ echo '\n#[CUSTOM] specify brew path\nexport PATH="/usr/local/bin:$PATH"' >> ~/.z
 
 # ZSH
 
-# add syntax highlighting
-brew install zsh-syntax-highlighting
-echo '\n#[CUSTOM] zsh syntax highlight config\nsource /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
 
 
 
 # source zsh configuration
 source ~/.zshrc
-
-#########  APPLICATION  #########
-
-# Quick Look plugins
-brew cask install \
-  qlcolorcode \
-  qlstephen \
-  qlmarkdown \
-  quicklook-json \
-  qlprettypatch \
-  quicklook-csv \
-  betterzip \
-  webpquicklook \
-  suspicious-package
-
-
-# Common Application
-brew cask install \
-  alfred \                     # keyboard shortcut
-  caffeine \                   # awake mac
-  cheatsheet \                 # application shortcut
-  chromium \                   # browser
-  dropbox \                    # toolbox
-  google-chrome \              # browser
-  istat-menus \                # system monitoring
-  keka \                       # zip & unzip utility
-  pdf-expert \                 # pdf editor
-  proxifier \                  # proxy
-  wechat \                     # communication
-  vlc \                        # video player
-  1password                    # password management
-
-
-# Application Toolkit
-brew cask install \
-  anaconda \                   # python environment
-  docker \                     # docker
-  intellij-idea-ce \           # ide
-  iterm2 \                     # terminal
-  keepassx \                   # store encrypted password locally
-  sourcetree \                 # version control
-  sublime-text \               # text editor
-  virtualbox \                 # virtual envrionment
-  visual-studio-code           # text editor
-
-
-# Command Line Toolkit
-brew install -with-default-names \
-  aria2 \                      # download utility
-  awscli \                     # aws cli
-  coreutils \
-  findutils \
-  gawk \
-  gnutls \
-  gnu-getopt \
-  gnu-indent \
-  gnu-tar \
-  gnu-sed \
-  htop \                       # system monitoring
-  imagemagick \                # image utility
-  jq \                         # json parse utility
-  mas \                        # mac app store in command line
-  screen \                     # simulation
-  tmux \                       # simulation
-  tree \                       # directory / file
-  wget \                       # download utility
-  xclip \                      # clipboard utility
-  you-get                      # video download utility
-
-
-#########  MAC APP STORE  #########
-
-mas install \
-  497799835 \                   # Xcode
-  540348655 \                   # Monosnap: Screenshot Utility
-  1176895641 \                  # Spark: EMail Client
-
